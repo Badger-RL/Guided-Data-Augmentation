cfe47a39c80ed3a19891d22bd00d8e10b42396e6
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-02-17 17:44:02 -0600

    Register PushBallToGoal-v0

---
+++ src/custom-envs/custom_envs/push_ball_to_goal.py
@@ -0,0 +1,128 @@
+import gym
+import numpy as np
+import warnings
+
+from custom_envs.base import BaseEnv
+
+warnings.filterwarnings("ignore")
+
+LENGTH = 500
+TRAINING_STEPS = 1000000
+
+
+class PushBallToGoalEnv(BaseEnv):
+    def __init__(self):
+        super().__init__()
+
+        """
+        OBSERVATION SPACE:
+            - x-cordinate of robot with respect to target
+            - y-cordinate of robot with respect to target
+            - sin(Angle between robot and target)
+            - cos(Angle between robot and target)
+        """
+        observation_space_size = 12
+
+        observation_space_low = -1 * np.ones(observation_space_size)
+        observation_space_high = np.ones(observation_space_size)
+        self.observation_space = gym.spaces.Box(
+            observation_space_low, observation_space_high
+        )
+
+        self.reset()
+
+    def reset(self):
+        self.time = 0
+
+        self.displacement_coef = 0.2
+
+        self.contacted_ball = False
+
+        self.robot_x = np.random.uniform(-3500, 3500)
+        self.robot_y = np.random.uniform(-2500, 2500)
+        self.robot_angle = np.random.uniform(0, 2 * np.pi)
+
+        self.target_x = np.random.uniform(-2500, 2500)
+        self.target_y = np.random.uniform(-2000, 2000)
+
+        self.goal_x = 4800
+        self.goal_y = 0
+
+        self.dummy1_x = (self.target_x + self.goal_x) / 2
+        self.dummy1_y = (self.target_y + self.goal_y) / 2
+
+        self.dummy2_x = (self.target_x + self.robot_x) / 2
+        self.dummy2_y = (self.target_y + self.robot_y) / 2
+
+        self.update_goal_value()
+
+        robot_location = np.array([self.robot_x, self.robot_y])
+        target_location = np.array([self.target_x, self.target_y])
+        self.initial_distance = np.linalg.norm(target_location - robot_location)
+
+        return self._observe_state()
+
+
+    def _observe_state(self):
+
+        self.update_target_value()
+        self.update_goal_value()
+
+        return np.array(
+            [
+                (self.dummy1_x - self.robot_x) / 9000,
+                (self.dummy1_y - self.robot_y) / 6000,
+                (self.dummy2_x - self.robot_x) / 9000,
+                (self.dummy2_y - self.robot_y) / 6000,
+                (self.target_x - self.robot_x) / 9000,
+                (self.target_y - self.robot_y) / 6000,
+                (self.goal_x - self.target_x) / 9000,
+                (self.goal_y - self.target_y) / 6000,
+                np.sin(self.relative_angle - self.robot_angle),
+                np.cos(self.relative_angle - self.robot_angle),
+                np.sin(self.goal_relative_angle - self.robot_angle),
+                np.cos(self.goal_relative_angle - self.robot_angle),
+            ]
+        )
+
+    def observe_global_state(self):
+        return [
+            self.robot_x / 9000,
+            self.robot_y / 6000,
+            self.target_x / 9000,
+            self.target_y / 6000,
+            self.dummy1_x / 9000,
+            self.dummy1_y / 6000,
+            self.dummy2_x / 9000,
+            self.dummy2_y / 6000,
+            np.sin(self.robot_angle),
+            np.cos(self.robot_angle),
+        ]
+
+    def set_abstract_state(self, obs):
+
+        self.target_x = self.goal_x - obs[6]*9000
+        self.target_y = self.goal_y - obs[7]*6000
+        self.robot_x = self.target_x - obs[4]*9000
+        self.robot_y = self.target_y - obs[5]*6000
+
+        relative_x = self.target_x - self.robot_x
+        relative_y = self.target_y - self.robot_y
+        relative_angle = np.arctan2(relative_y*6000, relative_x*9000)
+        if relative_angle < 0:
+            relative_angle += 2*np.pi
+
+        relative_angle_minus_robot_angle = np.arctan2(obs[8], obs[9])
+        if relative_angle_minus_robot_angle < 0:
+            relative_angle_minus_robot_angle += 2*np.pi
+
+        self.robot_angle = relative_angle - relative_angle_minus_robot_angle
+        if self.robot_angle < 0:
+            self.robot_angle += 2*np.pi
+
+        # dummy is FIXED throughout an episode
+        self.dummy1_x = obs[0] + self.robot_x
+        self.dummy1_y = obs[1] + self.robot_y
+        self.dummy2_x = obs[2] + self.robot_x
+        self.dummy2_y = obs[3] + self.robot_y
+
