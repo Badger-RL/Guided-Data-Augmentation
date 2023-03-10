cbb31a8b62679d353168c3841f0d52cda93bba3d
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-03-09 21:05:03 -0600

    More aug trajectories

--- src/generate/generate_guided_trajectory_datasets.py
+++ src/generate/generate_guided_trajectory_datasets.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     # os.chdir('generate')
     for i in range(5):
         os.system(
-            f'python ./generate_guided_trajectory_dataset.py --seed {i}'
+            f'python ./generate_guided_trajectory_dataset.py --seed {i} --aug-ratio 100'
             f' --observed-dataset-path ../datasets/expert/trajectories/{i}.hdf5'
             f' --save-dir ../datasets/expert/trajectories/guided'
             f' --save-name {i}.hdf5')
