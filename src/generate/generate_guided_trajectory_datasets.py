0239b62b364e2575759c9b15e00ff9b3dcc37116
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-03-21 20:39:13 -0500

    guided 10k, 50k, 100k

--- src/generate/generate_guided_trajectory_datasets.py
+++ src/generate/generate_guided_trajectory_datasets.py
@@ -2,10 +2,11 @@ import os
 
 if __name__ == "__main__":
 
-    # os.chdir('generate')
     for i in range(5):
-        os.system(
-            f'python ./generate_guided_trajectory_dataset.py --seed {i} --aug-ratio 100'
-            f' --observed-dataset-path ../datasets/expert/trajectories/{i}.hdf5'
-            f' --save-dir ../datasets/expert/trajectories/guided'
-            f' --save-name {i}.hdf5')
+        for aug_ratio in [99, 199]:
+            aug_dataset_size = 500 + aug_ratio*500
+            os.system(
+                f'python ./generate_guided_trajectory_dataset.py --seed {i} --aug-ratio {aug_ratio}'
+                f' --observed-dataset-path ../datasets/expert/trajectories/{i}.hdf5'
+                f' --save-dir ../datasets/expert/trajectories/guided'
+                f' --save-name {i}_{aug_dataset_size}.hdf5')
