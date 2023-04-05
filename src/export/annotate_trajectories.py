64e12937a2a267b919290f58a3c1d58fd390b920
John U. Balis <phobossystems@gmail.com>
2023-04-05 15:25:14 -0500

    added setstate and also updated annotate trajectories

--- src/export/annotate_trajectories.py
+++ src/export/annotate_trajectories.py
@@ -1,8 +1,12 @@
 import json
 import sys
-
+from custom_envs.push_ball_to_goal import PushBallToGoalEnv
+import h5py
+import numpy as np
 
 #this file is for annotating trajectories returned from the physical robots with rewards and terminals.
+
+env = PushBallToGoalEnv()
 
 
 def annotate_trajectories(trajectories):
@@ -10,11 +14,29 @@ def annotate_trajectories(trajectories):
     trajectories["terminals"][-1] = True
     trajectories["observations"] = [observation[4:] for observation in trajectories["observations"]]
     trajectories["next_observations"] = [observation[4:] for observation in trajectories["next_observations"]]
+    trajectories["rewards"] = []
+    for observation in trajectories["next_observations"]:
+        env.set_state_from_obs(observation)
+        trajectories["rewards"].append(env.calculate_reward())
+
     return trajectories
+
+
+def npify(data):
+    for k in data:
+        if k in ['terminals', 'timeouts']:
+            dtype = np.bool_
+        else:
+            dtype = np.float32
+
+        data[k] = np.array(data[k], dtype=dtype)
+
 
 if __name__ == "__main__":
 
-    if not len(sys.argv) == 3:
+    
+
+    if not len(sys.argv) >= 3:
         print("usage: python3 ./annotate_trajectories.py <input_path> <output_path>")
         exit()
 
@@ -26,7 +48,14 @@ if __name__ == "__main__":
 
     print(dataset)
 
-    with open(sys.argv[2], 'w') as output_file:
-        json.dump(dataset, output_file)
+
+    if "--json" in sys.argv:
+        with open(sys.argv[2], 'w') as output_file:
+            json.dump(dataset, output_file)
+    else:
+        out_file = h5py.File(sys.argv[2], 'w')
+        npify(dataset)
+        for k in dataset:
+            out_file.create_dataset(k, data=dataset[k], compression='gzip')
 
     
