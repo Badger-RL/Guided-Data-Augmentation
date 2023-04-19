e8afc97b7b36229bf9f7ccf7a2328fb1ddb9190d
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-04-19 16:04:04 -0500

    physical random aug datasets

--- src/viz/physical_aug_random.py
+++ src/viz/physical_aug_random.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     os.system(
         f'python ./visualize_dataset.py  '
         f' --dataset-path ../datasets/physical/aug_uniform/10_episodes_200k.hdf5'
-        f' --save-dir ./figures/expert/'
-        f' --save-name aug_uniform.png'
+        f' --save-dir ./figures/physical/aug_random/'
+        f' --save-name 10_episodes_200k.png'
     )
