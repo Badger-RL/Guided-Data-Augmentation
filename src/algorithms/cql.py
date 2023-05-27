9cbe848b1acce3e298d1867978258e782996ee54
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-05-27 12:13:56 -0500

    modularize CQL, TD3+BC

--- src/algorithms/cql.py
+++ src/algorithms/cql.py
@@ -24,22 +24,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 import wandb
 
-from src.algorithms.utils import get_latest_run_id, make_save_dir, load_dataset, wandb_init, set_seed
+from src.algorithms.utils import get_latest_run_id, make_save_dir, load_dataset, wandb_init, set_seed, TrainConfigBase, \
+    TensorBatch, train_base, soft_update
 
 sys.path.append("..")
 
-TensorBatch = List[torch.Tensor]
+
 
 @dataclass
-class TrainConfig:
-    # Experiment
-    device: str = "cpu"
-    env: str = "maze2d-umaze-v1"  # OpenAI gym environment name
-    seed: int = 0  # Sets Gym, PyTorch and Numpy seeds
-    eval_freq: int = int(500)  # How often (time steps) we evaluate
-    n_episodes: int = 10  # How many episodes run during evaluation
-    max_timesteps: int = int(100000)  # Max time steps to run environment
-    load_model: str = ""  # Model load file name, "" doesn't load
+class TrainConfig(TrainConfigBase):
     # CQL
     buffer_size: int = 2_000_000  # Replay buffer size
     batch_size: int = 256  # Batch size for all networks
@@ -55,8 +48,8 @@ class TrainConfig:
     target_update_period: int = 1  # Frequency of target nets updates
     cql_n_actions: int = 10  # Number of sampled actions
     cql_importance_sample: bool = True  # Use importance sampling
-    cql_lagrange: bool = False  # Use Lagrange version of CQL
-    cql_target_action_gap: float = -1.0  # Action gap
+    cql_lagrange: bool = True  # Use Lagrange version of CQL
+    cql_target_action_gap: float = 10  # Action gap
     cql_temp: float = 1.0  # CQL temperature
     cql_min_q_weight: float = 10.0  # Minimal Q weight
     cql_max_target_backup: bool = False  # Use max target backup
@@ -65,163 +58,9 @@ class TrainConfig:
     orthogonal_init: bool = True  # Orthogonal initialization
     normalize: bool = True  # Normalize states
     normalize_reward: bool = False  # Normalize reward
-    # Wandb logging
-    use_wandb: bool = False
-    project: str = "CORL"
-    group: str = "CQL-D4RL"
-    name: str = None
-    save_dir: str = "results"
-    run_id: str = None
-    save_policy: bool = True
 
     def __post_init__(self):
         self.name = self.name
-
-def soft_update(target: nn.Module, source: nn.Module, tau: float):
-    for target_param, source_param in zip(target.parameters(), source.parameters()):
-        target_param.data.copy_((1 - tau) * target_param.data + tau * source_param.data)
-
-
-def compute_mean_std(states: np.ndarray, eps: float) -> Tuple[np.ndarray, np.ndarray]:
-    mean = states.mean(0)
-    std = states.std(0) + eps
-    return mean, std
-
-
-def normalize_states(states: np.ndarray, mean: np.ndarray, std: np.ndarray):
-    return (states - mean) / std
-
-
-def wrap_env(
-    env: gym.Env,
-    state_mean: Union[np.ndarray, float] = 0.0,
-    state_std: Union[np.ndarray, float] = 1.0,
-    reward_scale: float = 1.0,
-) -> gym.Env:
-    # PEP 8: E731 do not assign a lambda expression, use a def
-    def normalize_state(state):
-        return (
-            state - state_mean
-        ) / state_std  # epsilon should be already added in std.
-
-    def scale_reward(reward):
-        # Please be careful, here reward is multiplied by scale!
-        return reward_scale * reward
-
-    env = gym.wrappers.TransformObservation(env, normalize_state)
-    if reward_scale != 1.0:
-        env = gym.wrappers.TransformReward(env, scale_reward)
-    return env
-
-
-class ReplayBuffer:
-    def __init__(
-        self,
-        state_dim: int,
-        action_dim: int,
-        buffer_size: int,
-        device: str = "cpu",
-    ):
-        self._buffer_size = buffer_size
-        self._pointer = 0
-        self._size = 0
-
-        self._states = torch.zeros(
-            (buffer_size, state_dim), dtype=torch.float32, device=device
-        )
-        self._actions = torch.zeros(
-            (buffer_size, action_dim), dtype=torch.float32, device=device
-        )
-        self._rewards = torch.zeros((buffer_size, 1), dtype=torch.float32, device=device)
-        self._next_states = torch.zeros(
-            (buffer_size, state_dim), dtype=torch.float32, device=device
-        )
-        self._dones = torch.zeros((buffer_size, 1), dtype=torch.float32, device=device)
-        self._device = device
-
-    def _to_tensor(self, data: np.ndarray) -> torch.Tensor:
-        return torch.tensor(data, dtype=torch.float32, device=self._device)
-
-    # Loads data in d4rl format, i.e. from Dict[str, np.array].
-    def load_d4rl_dataset(self, data: Dict[str, np.ndarray]):
-        if self._size != 0:
-            raise ValueError("Trying to load data into non-empty replay buffer")
-        n_transitions = data["observations"].shape[0]
-        if n_transitions > self._buffer_size:
-            raise ValueError(
-                "Replay buffer is smaller than the dataset you are trying to load!"
-            )
-        self._states[:n_transitions] = self._to_tensor(data["observations"])
-        self._actions[:n_transitions] = self._to_tensor(data["actions"])
-        self._rewards[:n_transitions] = self._to_tensor(data["rewards"][..., None])
-        self._next_states[:n_transitions] = self._to_tensor(data["next_observations"])
-        self._dones[:n_transitions] = self._to_tensor(data["terminals"][..., None])
-        self._size += n_transitions
-        self._pointer = min(self._size, n_transitions)
-
-        print(f"Dataset size: {n_transitions}")
-
-    def sample(self, batch_size: int) -> TensorBatch:
-        indices = np.random.randint(0, min(self._size, self._pointer), size=batch_size)
-        states = self._states[indices]
-        actions = self._actions[indices]
-        rewards = self._rewards[indices]
-        next_states = self._next_states[indices]
-        dones = self._dones[indices]
-        return [states, actions, rewards, next_states, dones]
-
-    def add_transition(self):
-        # Use this method to add new data into the replay buffer during fine-tuning.
-        # I left it unimplemented since now we do not do fine-tuning.
-        raise NotImplementedError
-
-
-@torch.no_grad()
-def eval_actor(
-    env: gym.Env, actor: nn.Module, device: str, n_episodes: int, seed: int
-) -> np.ndarray:
-    env.seed(seed)
-    actor.eval()
-    episode_rewards = []
-    successes = []
-    for _ in range(n_episodes):
-        state, done = env.reset(), False
-        episode_reward = 0.0
-        while not done:
-            action = actor.act(state, device)
-            state, reward, done, info = env.step(action)
-            episode_reward += reward
-        episode_rewards.append(episode_reward)
-        if 'is_success' in info:
-            successes.append(info['is_success'])
-
-    actor.train()
-    return np.asarray(episode_rewards), np.array(successes)
-
-
-def return_reward_range(dataset, max_episode_steps):
-    returns, lengths = [], []
-    ep_ret, ep_len = 0.0, 0
-    for r, d in zip(dataset["rewards"], dataset["terminals"]):
-        ep_ret += float(r)
-        ep_len += 1
-        if d or ep_len == max_episode_steps:
-            returns.append(ep_ret)
-            lengths.append(ep_len)
-            ep_ret, ep_len = 0.0, 0
-    lengths.append(ep_len)  # but still keep track of number of steps
-    assert sum(lengths) == len(dataset["rewards"])
-    return min(returns), max(returns)
-
-
-def modify_reward(dataset, env_name, max_episode_steps=1000):
-    if any(s in env_name for s in ("halfcheetah", "hopper", "walker2d")):
-        min_ret, max_ret = return_reward_range(dataset, max_episode_steps)
-        dataset["rewards"] /= max_ret - min_ret
-        dataset["rewards"] *= max_episode_steps
-    elif "antmaze" in env_name:
-        dataset["rewards"] -= 1.0
-
 
 def extend_and_repeat(tensor: torch.Tensor, dim: int, repeat: int) -> torch.Tensor:
     return tensor.unsqueeze(dim).repeat_interleave(repeat, dim=dim)
@@ -792,55 +631,11 @@ class ContinuousCQL:
 
 
 @pyrallis.wrap()
-def train( config: TrainConfig):
-
-    # create save directories
-    make_save_dir(config=config)
-
-    # setup wandb logging
-    if config.use_wandb:
-        wandb_init(config)
-
+def train(config: TrainConfig):
     # make env
     env = gym.make(config.env)
-
-    # load dataset
-    dataset = load_dataset(config=config, env=env)
-
-    # preprocess dataset
-    if config.normalize_reward:
-        modify_reward(dataset, config.env)
-
-    if config.normalize:
-        state_mean, state_std = compute_mean_std(dataset["observations"], eps=1e-3)
-    else:
-        state_mean, state_std = 0, 1
-
-    dataset["observations"] = normalize_states(
-        dataset["observations"], state_mean, state_std
-    )
-    dataset["next_observations"] = normalize_states(
-        dataset["next_observations"], state_mean, state_std
-    )
-
-    # load env
-    env = gym.make(config.env)
-    env = wrap_env(env, state_mean=state_mean, state_std=state_std)
-
-    # create replay buffer
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
-    replay_buffer = ReplayBuffer(
-        state_dim,
-        action_dim,
-        config.buffer_size,
-        config.device,
-    )
-    replay_buffer.load_d4rl_dataset(dataset)
-
-    # Set seeds
-    seed = config.seed
-    set_seed(seed, env)
 
     # create nets
     critic_1 = FullyConnectedQFunction(state_dim, action_dim, config.orthogonal_init).to(config.device)
@@ -883,91 +678,9 @@ def train( config: TrainConfig):
         "cql_clip_diff_max": config.cql_clip_diff_max,
     }
 
-    print("---------------------------------------")
-    print(f"Training CQL, Env: {config.env}, Seed: {seed}")
-    print("---------------------------------------")
-
     # Initialize actor
     trainer = ContinuousCQL(**kwargs)
-
-    # load policy if applicable
-    if config.load_model != "":
-        policy_file = Path(config.load_model)
-        trainer.load_state_dict(torch.load(policy_file))
-        actor = trainer.actor
-
-    # local logging
-    log_evaluations = defaultdict(lambda: [])
-    log_stats = defaultdict(lambda: [])
-    best_eval_score = -np.inf
-
-    for t in range(int(config.max_timesteps)+1):
-        batch = replay_buffer.sample(config.batch_size)
-        batch = [b.to(config.device) for b in batch]
-        stats_dict = trainer.train(batch)
-
-        # log training statistics
-        if config.use_wandb and t % 100 == 0:
-            wandb.log(stats_dict, step=trainer.total_it)
-
-        # evalutate agent
-        if (t) % config.eval_freq == 0:
-            print(f"Time steps: {t}")
-            eval_scores, eval_successes = eval_actor(
-                env,
-                actor,
-                device=config.device,
-                n_episodes=config.n_episodes,
-                seed=config.seed,
-            )
-            eval_score = eval_scores.mean()
-            eval_success_rate = eval_successes.mean()
-            normalized_eval_score = eval_score#env.get_normalized_score(eval_score) * 100.0
-            print("---------------------------------------")
-            print(
-                f"Evaluation over {config.n_episodes} episodes: "
-                f"{eval_score:.3f} , D4RL score: {normalized_eval_score:.3f} "
-                f"Success rate: {eval_success_rate:.3f}"
-            )
-            print("---------------------------------------")
-
-            # log evaluations
-            log_evaluations['timestep'].append(t)
-            log_evaluations['return'].append(eval_score)
-            log_evaluations['success_rate'].append(eval_success_rate)
-            np.savez(os.path.join(config.save_dir, "evaluations.npz"), **log_evaluations)
-
-            # log training stats
-            log_stats['timestep'].append(t)
-            for key, val in stats_dict.items():
-                log_stats[key].append(val)
-            np.savez(os.path.join(config.save_dir, "stats.npz"), **log_stats)
-
-            # save current model
-            torch.save(
-                trainer.state_dict(),
-                os.path.join(config.save_dir, f"model.pt"),
-            )
-
-            # save best model
-            if config.save_policy:
-                if eval_score > best_eval_score:
-                    best_eval_score = eval_score
-                    torch.save(
-                        trainer.state_dict(),
-                        os.path.join(config.save_dir, f"best_model.pt"),
-                    )
-
-
-            if config.use_wandb:
-                wandb.log(
-                    {"d4rl_normalized_score": normalized_eval_score},
-                    step=trainer.total_it,
-                )
-                wandb.log(
-                    {"success_rate": eval_success_rate},
-                    step=trainer.total_it,
-                )
+    train_base(config, env, trainer)
 
 
 
