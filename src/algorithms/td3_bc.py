b44a544eea6661aebb2104d645f4611959f68a37
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-05-29 14:07:16 -0500

    smaller network for td3bc

--- src/algorithms/td3_bc.py
+++ src/algorithms/td3_bc.py
@@ -183,11 +183,11 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
 
         self.net = nn.Sequential(
-            nn.Linear(state_dim, 256),
+            nn.Linear(state_dim, 64),
             nn.ReLU(),
-            nn.Linear(256, 256),
+            nn.Linear(64, 64),
             nn.ReLU(),
-            nn.Linear(256, action_dim),
+            nn.Linear(64, action_dim),
             nn.Tanh(),
         )
 
