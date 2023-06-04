6c439317c03740b16a0345bf3d56db31e37883c8
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-06-04 14:11:15 -0500

    command scripts

--- src/condor/common.py
+++ src/condor/common.py
@@ -1,18 +1,66 @@
-def gen_command(
+
+
+def gen_cql(
         save_dir,
+        env_id,
+        dataset_name=None,
+        max_timesteps=int(100e3),
+        eval_freq=5000,
+        qf_lr=3e-4,
+        policy_lr=3e-5,
+        gap=5,
+):
+    command = f'python -u algorithms/cql.py --max_timesteps {max_timesteps} --eval_freq {eval_freq}' \
+              f' --save_dir {save_dir} ' \
+              f' --env {env_id}' \
+              f' --qf_lr {qf_lr}' \
+              f' --policy_lr {policy_lr}' \
+              f' --cql_target_action_gap {gap}'
+
+    if dataset_name:
+        command += f' --dataset_name {dataset_name}'
+
+    return command
+
+def gen_td3_bc(
+        save_dir,
+        env_id,
         dataset_name,
         max_timesteps=int(100e3),
         eval_freq=5000,
-        qf_lr=3e-06,
-        policy_lr=3e-06,
-        soft_target_update_rate=0.05,
-        target_update_period=10
+        actor_lr=3e-4,
+        critic_lr=3e-4,
+        alpha=2.5
 ):
-    command = f'python algorithms/cql.py --max_timesteps {max_timesteps} --eval_freq {eval_freq}' \
+    command = f'python -u algorithms/td3_bc.py --max_timesteps {max_timesteps} --eval_freq {eval_freq}' \
               f' --save_dir {save_dir} ' \
-              f' --dataset_name {dataset_name}' \
-              f' --qf_lr {qf_lr}' \
-              f' --policy_lr {policy_lr}' \
-              f' --soft_target_update_rate {soft_target_update_rate}' \
-              f' --target_update_period {target_update_period}' 
+              f' --env {env_id}' \
+              f' --actor_lr {actor_lr}' \
+              f' --critic_lr {critic_lr}' \
+              f' --alpha {alpha}'
+
+    if dataset_name:
+        command += f' --dataset_name {dataset_name}'
+
+    return command
+
+
+def gen_awac(
+        save_dir,
+        env_id,
+        dataset_name,
+        max_timesteps=int(100e3),
+        eval_freq=10000,
+        lr=3e-4,
+        lmbda=1,
+):
+    command = f'python -u algorithms/awac.py --max_timesteps {max_timesteps} --eval_freq {eval_freq}' \
+              f' --save_dir {save_dir} ' \
+              f' --env {env_id}' \
+              f' --learning_rate {lr}' \
+              f' --lambda {lmbda}' \
+
+    if dataset_name:
+        command += f' --dataset_name {dataset_name}'
+
     return command
