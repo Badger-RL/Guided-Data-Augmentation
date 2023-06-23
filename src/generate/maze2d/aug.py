97c8155d8df6ef06138705cf0a8538748e4bbe17
Nicholas Corrado <nicholascorrado@Nicholass-Air.lan1>
2023-06-22 23:40:56 -0500

    Restore dataset gen defaults

--- src/generate/maze2d/aug.py
+++ src/generate/maze2d/aug.py
@@ -1,7 +1,8 @@
 import os
 
-for m in [1,2,4]:
+for m in [1]:
     for maze in ['umaze', 'medium', 'large']:
         env_id = f'maze2d-{maze}-v1'
-        for aug_func in ['random', 'guided']:
-            os.system(f'python ../augment_dataset.py --env-id {env_id} --aug-func {aug_func} --save-dir ../../datasets/{env_id}/{aug_func}') #--observed-dataset-path ../../datasets/{env_id}/no_aug-sparse.hdf5')
+        for aug_func in ['no_aug', 'random', 'guided', 'mixed']:
+            print(env_id, m)
+            os.system(f'python ../augment_dataset.py -m {m} --env-id {env_id} --aug-func {aug_func} --save-dir ../../datasets/{env_id}/{aug_func}')
