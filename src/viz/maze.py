5c7805df1151aa2f1c2fa7f732cd6aa03d333ca0
Nicholas Corrado <nicholascorrado@Nicholass-Air.lan1>
2023-06-22 23:33:24 -0500

    edits to maze2d figures"

--- src/viz/maze.py
+++ src/viz/maze.py
@@ -9,39 +9,35 @@ from matplotlib import pyplot as plt
 from algorithms.utils import load_dataset
 
 
-env_id = 'maze2d-umaze-v1'
-# env_id = 'maze2d-medium-v1'
-env_id = 'maze2d-medium-v1'
+for env_id in ['maze2d-umaze-v1', 'maze2d-medium-v1', 'maze2d-large-v1']:
+    for aug in ['mixed']:
+        plt.figure(figsize=(12, 12))
 
-env = gym.make(env_id)
+        dataset_name = f'../datasets/{env_id}/{aug}/m_1.hdf5'
 
-dataset_name = None
-# dataset_name = f"../datasets/{env_id}/no_aug.hdf5"
-dataset_name = f'../datasets/{env_id}/random/m_1.hdf5'
-
-
-for env_id in ['maze2d-umaze-v1', 'maze2d-medium-v1']:
-    plt.figure(figsize=(12,12))
-    for aug in ['no_aug', 'guided']:
-        dataset_name = f'../datasets/{env_id}/guided/m_1.hdf5'
-
+        # local dataset
         dataset = {}
-        if dataset_name:
-            # local dataset
-            data_hdf5 = h5py.File(dataset_name, "r")
-            for key in data_hdf5.keys():
-                dataset[key] = np.array(data_hdf5[key])
-        else:
-            # remote dataset
-            dataset = d4rl.qlearning_dataset(env)
-        # dataset = d4rl.qlearning_dataset(env, dataset=dataset)
+        data_hdf5 = h5py.File(dataset_name, "r")
+        for key in data_hdf5.keys():
+            dataset[key] = np.array(data_hdf5[key])
 
-        if aug == 'no_aug':
-            start = int(0e6)
-            end = start + int(5e3)
-        else:
-            start = len(dataset['observations']) - int(3e3)
-            end = start + int(3e3)
+        # plot no_aug
+        start = int(0e6)
+        end = start + int(5e3)
+
+        observations = dataset['observations'][start:end]
+        next_observations = dataset['next_observations'][start:end]
+        rewards = dataset['rewards'][start:end]
+        at_goal = rewards > 0
+
+        x = observations[:, 0]
+        y = observations[:, 1]
+        plt.scatter(x, y, alpha=0.5)
+        plt.scatter(x[at_goal], y[at_goal], alpha=0.5, color='g')
+
+        # plot aug
+        start = len(dataset['observations']) - int(3e3)
+        end = start + int(3e3)
         observations = dataset['observations'][start:end]
         next_observations = dataset['next_observations'][start:end]
         rewards = dataset['rewards'][start:end]
@@ -52,70 +48,21 @@ for env_id in ['maze2d-umaze-v1', 'maze2d-medium-v1']:
         next_x = next_observations[:, 0]
         next_y = next_observations[:, 1]
 
-        if aug == 'no_aug':
-            plt.scatter(x, y, alpha=0.5)
-        else:
-            u = next_x - x
-            v = next_y - y
-            norm = np.sqrt(u**2 + v**2)
-            u /= norm
-            v /= norm
+        u = next_x - x
+        v = next_y - y
+        norm = np.sqrt(u**2 + v**2)
+        u /= norm
+        v /= norm
 
-            plt.quiver(x, y, u, v)
-        plt.scatter(x[at_goal],y[at_goal], alpha=0.3, color='g')
+        plt.quiver(x, y, u, v)
+        plt.scatter(next_x[at_goal],next_y[at_goal], alpha=0.5, color='g')
 
-    plt.title(f'{env_id}: {aug}', fontsize=24)
-    plt.xlabel('x position', fontsize=24)
-    plt.ylabel('y position', fontsize=24)
-    # plt.show()
-    os.makedirs(f'figures/{env_id}', exist_ok=True)
-    plt.savefig(f'figures/{env_id}/guided.png')
-
-    plt.figure(figsize=(12, 12))
-    for aug in ['no_aug', 'random']:
-        dataset_name = f'../datasets_good/{env_id}/random/m_1.hdf5'
-        dataset = {}
-        if dataset_name:
-            # local dataset
-            data_hdf5 = h5py.File(dataset_name, "r")
-            for key in data_hdf5.keys():
-                dataset[key] = np.array(data_hdf5[key])
-        else:
-            # remote dataset
-            dataset = d4rl.qlearning_dataset(env)
-        # dataset = d4rl.qlearning_dataset(env, dataset=dataset)
-
-        if aug == 'no_aug':
-            start = int(0e6)
-            end = start + int(5e3)
-        else:
-            start = len(dataset['observations']) - int(3e3)
-            end = start + int(3e3)
-        observations = dataset['observations'][start:end]
-        next_observations = dataset['next_observations'][start:end]
-        rewards = dataset['rewards'][start:end]
-        at_goal = rewards > 0
-
-
-        x = observations[:, 0]
-        y = observations[:, 1]
-        next_x = next_observations[:, 0]
-        next_y = next_observations[:, 1]
-
-        if aug == 'no_aug':
-            plt.scatter(x, y, alpha=0.5)
-        else:
-            u = next_x - x
-            v = next_y - y
-            norm = np.sqrt(u ** 2 + v ** 2)
-            u /= norm
-            v /= norm
-
-            plt.quiver(x, y, u, v)
-        plt.scatter(x[at_goal], y[at_goal], alpha=0.3, color='g')
-
-    plt.title(f'{env_id}: {aug}', fontsize=24)
-    plt.xlabel('x position', fontsize=24)
-    plt.ylabel('y position', fontsize=24)
-    # plt.show()
-    plt.savefig(f'figures/{env_id}/random.png')
+        plt.title(f'{env_id}: {aug}', fontsize=24)
+        plt.xlabel('x position', fontsize=24)
+        plt.ylabel('y position', fontsize=24)
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        plt.tight_layout()
+        plt.show()
+        # os.makedirs(f'figures/{env_id}', exist_ok=True)
+        # plt.savefig(f'figures/{env_id}/{aug}.png')
