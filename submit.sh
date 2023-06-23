1087c13a21803f35fc9812255aa837e4b1f1f857
ncorrado <ncorrado@submit-1.chtc.wisc.edu>
2023-06-23 11:46:35 -0500

    Update submission scripts

--- submit.sh
+++ submit.sh
@@ -1,1 +1,2 @@
-condor_submit offlinerl.sub num_jobs=$2 commands_file=src/condor/commands/${2}
+mkdir -p results/$3
+condor_submit condor_execute.sub num_jobs=$2 commands_file=commands/${1} output_dir=results/$3
