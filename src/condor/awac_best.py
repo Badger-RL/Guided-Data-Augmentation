1b5c9f9d746bede8a8293591913fdbef46a0f85b
Nicholas Corrado <ncorrado@wisc.edu>
2023-07-07 12:24:12 -0500

    Antmaze Augmentations (#23)
    * Update things for Yuxiao
    
    * Fix antmaze augmentation func
    
    * antmaze figures
    
    * Bug fix; quaternion is stored as (qx, qy, qz, qw)
    
    * larger net
    
    * debugging
    
    * copmmand gen
    
    * update plotting
    
    ---------
    
    Co-authored-by: Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>

--- src/condor/awac_best.py
+++ src/condor/awac_best.py
@@ -1,6 +1,24 @@
 from condor.common import gen_cql, gen_awac
 
-PARAMS128 = {
+PARAMS = { # nl = 1, hd = 128
+    'maze2d-umaze-v1': {
+        'no_aug': 0.5,
+        'random': 0.5,
+        'guided': 0.5,
+    },
+    'maze2d-medium-v1': {
+        'no_aug': 0.25,
+        'random': 0.25,
+        'guided': 0.25,
+    },
+    'maze2d-large-v1': {
+        'no_aug': 0.25,
+        'random': 2,
+        'guided': 2,
+    },
+}
+
+PARAMS_LARGE = { # nl = 2, hd = 256
     'maze2d-umaze-v1': {
         'no_aug': 0.5,
         'random': 0.5,
@@ -12,9 +30,9 @@ PARAMS128 = {
         'guided': 2,
     },
     'maze2d-large-v1': {
-        'no_aug': 2,
-        'random': 2,
-        'guided': 1,
+        'no_aug': 0.5,
+        'random': 1,
+        'guided': 2,
     },
 }
 
@@ -37,34 +55,29 @@ if __name__ == "__main__":
     all_commands = ""
 
     for env_id in ['maze2d-umaze-v1', 'maze2d-medium-v1', 'maze2d-large-v1']:
+        # for env_id in ['antmaze-medium-diverse-v1', 'antmaze-large-diverse-v1', 'antmaze-umaze-diverse-v1']:
         for aug in ['no_aug', 'random', 'guided']:
-            lr = 3e-4
-            n_layers = 1
-            hidden_dims = 128
-            lmbda = PARAMS128[env_id][aug]
 
             m = 1
+            n_layers = 2
+            hidden_dims = 256
+            lr = 3e-4
+            lmbda = PARAMS_LARGE[env_id][aug]
+            save_dir = f'results/{aug}/{env_id}/awac/'
 
             if aug == 'no_aug':
                 dataset_name = None
             else:
                 dataset_name = f'/staging/ncorrado/datasets/{env_id}/{aug}/m_{m}.hdf5'
-            if aug == 'no_aug':
-                save_dir = f'results/{aug}/{env_id}/awac/'
-                batch_size = 256
-            else:
-                save_dir = f'results/{aug}/m_{m}/{env_id}/awac/'
-                batch_size = 256 * (m + 1)
-
 
             max_timesteps = int(1e6)
-            eval_freq = int(10e3)
+            eval_freq = int(20e3)
             command = f'python -u algorithms/awac.py --max_timesteps {max_timesteps} --eval_freq {eval_freq}' \
                       f' --save_dir {save_dir} ' \
                       f' --env {env_id}' \
                       f' --learning_rate {lr}' \
-                      f' --batch_size {batch_size}' \
                       f' --awac_lambda {lmbda}' \
+                      f' --n_layers {n_layers}' \
                       f' --hidden_dim {hidden_dims}'
 
             if dataset_name:
