49855cdae0477ffd24edb13e2210937fc4be2e6f
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-07-15 16:40:51 -0500

    physical trajectories viz

--- src/viz/physical_expert_trajectories.py
+++ src/viz/physical_expert_trajectories.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     for i in range(10):
         os.system(
             f'python ./visualize_dataset.py  '
-            f' --dataset-path ../datasets/physical/trajectories/{i}.hdf5'
-            f' --save-dir ./figures/expert_physical/'
-            f' --save-name {i}.png'
+            f' --dataset-path ../datasets/PushBallToGoal-v0/physical/trajectories/traj_{i}.hdf5'
+            f' --save-dir ./figures/PushBallToGoal-v0/physical/'
+            f' --save-name traj_{i}.png'
         )
