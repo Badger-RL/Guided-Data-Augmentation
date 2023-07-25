b8e09518865f8591e24912ae0173a394d73ffb3a
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-07-25 17:26:41 -0500

    refactor

--- src/augment/abstractsim/rotate_reflect_translate.py
+++ src/augment/abstractsim/rotate_reflect_translate.py
@@ -1,5 +1,4 @@
 import copy
-from typing import Dict
 
 import numpy as np
 
@@ -13,27 +12,12 @@ class RotateReflectTranslate(AbstractSimAugmentationFunction):
     def __init__(self, env, **kwargs):
         super().__init__(env=env, **kwargs)
 
-    def _augment(self,
-                 obs: np.ndarray,
-                 next_obs: np.ndarray,
-                 action: np.ndarray,
-                 reward: np.ndarray,
-                 done: np.ndarray,
-                 **kwargs,
-                 ):
+    def _is_valid_input(self, absolute_obs, absolute_next_obs):
+        if self.at_goal(absolute_obs[2], absolute_obs[3]) or self.at_goal(absolute_next_obs[2], absolute_next_obs[3]):
+            return False
+        return True
 
-        # random robot position
-        absolute_obs = obs.copy()
-        absolute_next_obs = next_obs.copy()
-
-        if self.at_goal(absolute_obs[2], absolute_obs[3]):
-            return None, None, None, None, None, None, None
-
-        aug_absolute_obs = copy.deepcopy(absolute_obs)
-        aug_absolute_next_obs = copy.deepcopy(absolute_next_obs)
-        aug_action = action.copy()
-        aug_done = done.copy()
-
+    def _rotate(self, aug_absolute_obs, aug_absolute_next_obs):
         theta = np.random.uniform(-np.pi/4, np.pi/4)
 
         M = np.array([
@@ -59,19 +43,21 @@ class RotateReflectTranslate(AbstractSimAugmentationFunction):
             next_robot_angle += 2 * np.pi
         aug_absolute_next_obs[4] += theta
 
-        if np.random.random() < 0.5:
-            aug_absolute_obs[1] *= -1
-            aug_absolute_next_obs[1] *= -1
-            aug_absolute_obs[3] *= -1
-            aug_absolute_next_obs[3] *= -1
-            aug_absolute_obs[4] *= -1
-            aug_absolute_next_obs[4] *= -1
-            aug_absolute_obs[6] += np.pi
-            aug_absolute_next_obs[6] += np.pi
+    def _reflect(self, aug_absolute_obs, aug_absolute_next_obs, aug_action):
+        aug_absolute_obs[1] *= -1
+        aug_absolute_next_obs[1] *= -1
+        aug_absolute_obs[3] *= -1
+        aug_absolute_next_obs[3] *= -1
+        aug_absolute_obs[4] *= -1
+        aug_absolute_next_obs[4] *= -1
+        aug_absolute_obs[6] += np.pi
+        aug_absolute_next_obs[6] += np.pi
 
-            aug_action[0] *= -1
-            aug_action[1] *= 1
-            aug_action[2] *= -1
+        aug_action[0] *= -1
+        aug_action[1] *= 1
+        aug_action[2] *= -1
+
+    def _translate(self, aug_absolute_obs, aug_absolute_next_obs):
 
         xmin = np.min([aug_absolute_obs[0], aug_absolute_next_obs[0], aug_absolute_obs[2], aug_absolute_next_obs[2]])
         ymin = np.min([aug_absolute_obs[1], aug_absolute_next_obs[1], aug_absolute_obs[3], aug_absolute_next_obs[3]])
@@ -81,12 +67,6 @@ class RotateReflectTranslate(AbstractSimAugmentationFunction):
         # Translate bottom left corner of the righter bounding box containing the robot and ball
         new_x = np.random.uniform(-4500, 4500-(xmax-xmin))
         new_y = np.random.uniform(-3000, 3000-(ymax-ymin))
-
-        # if new_x > 4500 and 750-(ymax-ymin) > -750:
-        #     new_y = np.random.uniform(-750, 750-(ymax-ymin))
-        # else:
-        #     new_y = np.random.uniform(-3000, 3000-(ymax-ymin))
-
 
         delta_x = new_x - xmin
         delta_y = new_y - ymin
@@ -101,8 +81,29 @@ class RotateReflectTranslate(AbstractSimAugmentationFunction):
         aug_absolute_next_obs[2] += delta_x
         aug_absolute_next_obs[3] += delta_y
 
-        aug_reward = self.env.calculate_reward_2(aug_absolute_obs, aug_absolute_next_obs)
+    def _augment(self,
+                 abs_obs: np.ndarray,
+                 abs_next_obs: np.ndarray,
+                 action: np.ndarray,
+                 reward: np.ndarray,
+                 done: np.ndarray,
+                 **kwargs,
+                 ):
 
+        if not self._is_valid_input(abs_obs, abs_next_obs):
+            return None, None, None, None, None, None, None
+
+        aug_absolute_obs = copy.deepcopy(abs_obs)
+        aug_absolute_next_obs = copy.deepcopy(abs_next_obs)
+        aug_action = action.copy()
+        aug_done = done.copy()
+
+        self._rotate(aug_absolute_obs, aug_absolute_next_obs)
+        if np.random.random() < 0.5:
+            self._reflect(aug_absolute_obs, aug_absolute_next_obs, aug_action)
+        self._translate(aug_absolute_obs, aug_absolute_next_obs)
+
+        aug_reward = self.env.calculate_reward_2(aug_absolute_obs, aug_absolute_next_obs)
         aug_obs = self._convert_to_relative_obs(aug_absolute_obs)
         aug_next_obs = self._convert_to_relative_obs(aug_absolute_next_obs)
 
