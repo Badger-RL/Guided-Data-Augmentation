7f0db9adccb79cc628926bbd01e1338683e4934c
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-07-29 12:40:44 -0500

    update expert datasets

--- src/generate/abstractsim/expert.py
+++ src/generate/abstractsim/expert.py
@@ -21,21 +21,32 @@ def reset_data():
             'terminals': [],
             'rewards': [],
             'next_observations': [],
+            'dones': [],
 
             'absolute_observations': [],
             'absolute_next_observations': []
             }
 
 
-def append_data(data, s, a, r, ns, done, abs_s, abs_ns):
+def append_data(data, s, a, r, ns, terminal, done, abs_s, abs_ns):
     data['observations'].append(s)
     data['next_observations'].append(ns)
     data['actions'].append(a)
     data['rewards'].append(r)
-    data['terminals'].append(done)
+    data['terminals'].append(terminal)
+    data['dones'].append(done)
 
     data['absolute_observations'].append(abs_s)
     data['absolute_next_observations'].append(abs_ns)
+
+def append_dataset(data, append_data):
+    data['observations'].append(append_data['observations'])
+    data['next_observations'].append(append_data['next_observations'])
+    data['actions'].append(append_data['actions'])
+    data['rewards'].append(append_data['rewards'])
+    data['terminals'].append(append_data['terminals'])
+    data['absolute_observations'].append(append_data['absolute_observations'])
+    data['absolute_next_observations'].append(append_data['absolute_next_observations'])
 
 def npify(data):
     for k in data:
@@ -51,6 +62,8 @@ def npify(data):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_samples', type=int, default=int(100e3), help='Num samples to collect')
+    parser.add_argument('--num_traj', type=int, default=5, help='Num trajectories to collect. Overrides num_samples')
+    parser.add_argument('--num_traj_success', type=int, default=5, help='Num trajectories to collect. Overrides num_samples')
     parser.add_argument('--policy-path', type=str, default='../../../src/results/PushBallToGoal-v0/rl_model_3200000_steps.zip')
     parser.add_argument('--norm-path', type=str, default='')
     parser.add_argument('--save-dir', type=str, help='file_name')
@@ -75,41 +88,47 @@ def main():
     ret = 0
     successes = []
     rets =[]
-    for _ in range(args.num_samples):
-        if args.random_actions:
-            act = env.action_space.sample()
-        else:
-            act = policy.predict(s)[0]
-        act[-1] = 0
-          
-        ns, r, done, info = env.step(act)
-        ret += r
-        if args.render:
-            env.render()
 
-        append_data(data, s, act, r, ns, info['terminated'], info['absolute_obs'], info['absolute_next_obs'])
-
-        if len(data['observations']) % 10000 == 0:
-            print(len(data['observations']))
-
-        ts += 1
-        if done:
-            s = env.reset()
-            num_episodes += 1
-
-
-            if info['is_success']:
-                successes.append(1)
-
+    # num_traj_success = 0
+    # num_traj = 0
+    # while num_traj < args.num_traj and num_traj_success < args.num_traj_success:
+    for episode_i in range(args.num_traj):
+        done = False
+        while not done:
+            if args.random_actions:
+                act = env.action_space.sample()
             else:
-                successes.append(0)
+                act = policy.predict(s)[0]
+            act[-1] = 0
 
-            rets.append(ret)
-            print(ts, ret)
-            ret = 0
-            ts = 0
-        else:
-            s = ns
+            ns, r, done, info = env.step(act)
+            ret += r
+            if args.render:
+                env.render()
+
+            append_data(data, s, act, r, ns, info['terminated'], done, info['absolute_obs'], info['absolute_next_obs'])
+
+            if len(data['observations']) % 10000 == 0:
+                print(len(data['observations']))
+
+            ts += 1
+            if done:
+                s = env.reset()
+                num_episodes += 1
+
+
+                if info['is_success']:
+                    successes.append(1)
+
+                else:
+                    successes.append(0)
+
+                rets.append(ret)
+                print(ts, ret, successes[-1])
+                ret = 0
+                ts = 0
+            else:
+                s = ns
 
     print(np.average(successes), np.std(successes)/np.sqrt(len(rets)), np.average(rets), np.std(rets)/np.sqrt(len(rets)))
     save_dir = args.save_dir
--- src/generate/abstractsim/expert_driver.py
+++ src/generate/abstractsim/expert_driver.py
@@ -4,8 +4,15 @@ import os
 # 40 893.5071630557165
 # 60 1195.693313336531
 # 72 1353.9820733017361
-for success_rate in [40, 60, 72]:
-    os.system('python3 expert.py --num_samples 500000 '
-              f' --policy-path ../../../src/policies/PushBallToGoal-v0/policy_{success_rate}.zip'
-              f' --save-dir ../../datasets/PushBallToGoal-v0'
-              f' --save-name no_aug_{success_rate}.hdf5 ')
+for success_rate in [40,72]:
+    for n in [10]:
+        os.system(f'python3 expert.py --num_traj {n} '
+                  f' --policy-path ../../../src/policies/PushBallToGoal-v0/policy_{success_rate}.zip'
+                  f' --save-dir ../../datasets/PushBallToGoal-v0'
+                  f' --save-name no_aug_{success_rate}_{n}k.hdf5')
+
+# for success_rate in [60, 40]:
+#     os.system('python3 expert.py --num_samples 500000 '
+#               f' --policy-path ../../../src/policies/PushBallToGoal-v0/policy_{success_rate}.zip'
+#               f' --save-dir ../../datasets/PushBallToGoal-v0'
+#               f' --save-name no_aug_{success_rate}_deterministic.hdf5')
