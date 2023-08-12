602fbead4c72924afc7648c7cd779ce4d99f92d2
Nicholas Corrado <nicholascorrado@Nicholass-Air.lan1>
2023-08-11 23:34:19 -0500

    update cql command gen'

--- src/condor/abstractsim/cql.py
+++ src/condor/abstractsim/cql.py
@@ -11,30 +11,33 @@ if __name__ == "__main__":
             #     aug = f'{aug}_{expert}'
                 aug = f'{aug}'
                 for gap in [5]:
-                        for nl in [1,2]:
-                            dataset_name = f'/staging/ncorrado/datasets/{env_id}/{aug}.hdf5'
+                        for nl in [1]:
+                            for hd in [256]:
+                                dataset_name = f'/staging/ncorrado/datasets/{env_id}/{aug}.hdf5'
 
-                            save_dir = f'results/{env_id}/{aug}/td3bc/nl_{nl}/gap_{gap}'
-                            hidden_dims = 256
+                                save_dir = f'results/{env_id}/{aug}/td3bc/nl_{nl}/gap_{gap}'
+                                hidden_dims = 256
 
-                            max_timesteps = int(1e6) if nl == 2 else int(1e6)
-                            eval_freq = int(20e4)
+                                max_timesteps = int(1e6) if nl == 2 else int(1e6)
+                                eval_freq = int(20e3)
 
-                            command = f'python -u algorithms/cql.py --max_timesteps {max_timesteps} --eval_freq {eval_freq}' \
-                                      f' --save_dir {save_dir} ' \
-                                      f' --env {env_id}' \
-                                      f' --cql_target_action_gap {gap}' \
-                                      f' --cql_min_q_weight 5'
-                                      # f' --qf_lr {qf_lr}' \
-                                      # f' --policy_lr {policy_lr}' \
+                                command = f'python -u algorithms/cql.py --max_timesteps {max_timesteps} --eval_freq {eval_freq}' \
+                                          f' --save_dir {save_dir} ' \
+                                          f' --env {env_id}' \
+                                          f' --cql_target_action_gap {gap}' \
+                                          f' --cql_min_q_weight 5' \
+                                          f' --n_layers {nl} --hidden_dims {hidden_dims}'
+                                          # f' --qf_lr {qf_lr}' \
+                                          # f' --policy_lr {policy_lr}' \
 
-                                      # f' --batch_size {batch_size}' \
+                                          # f' --batch_size {batch_size}' \
 
-                            if dataset_name:
-                                command += f' --dataset_name {dataset_name}'
+                                if dataset_name:
+                                    command += f' --dataset_name {dataset_name}'
 
-                            # command = command + f' --device cuda'
-                            # command = '4,9,' + command.replace(' ', '*')
-                            print(command)
-                            i+=1
-                            all_commands += command + '\n'
+                                mem, disk = MEMDISK[1][env_id]
+                                memdisk = f'{mem},{disk},'
+                                command = memdisk + command.replace(' ', '*')
+                                print(command)
+                                i+=1
+                                all_commands += command + '\n'
