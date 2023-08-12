1c5fe3b6ee828b35054b322d2dd274b981bac439
Nicholas Corrado <nicholascorrado@Nicholass-Air.lan1>
2023-08-11 23:34:48 -0500

    update cql command gen

--- src/condor/cql.py
+++ src/condor/cql.py
@@ -1,50 +1,46 @@
-from condor.common import gen_td3_bc, gen_cql
-from condor.td3_bc import MEMDISK
+from condor.common import gen_td3_bc
+from condor.utils import MEMDISK
 
 if __name__ == "__main__":
     all_commands = ""
+
     i = 0
+    for env_id in ['maze2d-umaze-v1', 'maze2d-medium-v1', 'maze2d-large-v1']:
+        for aug in ['no_aug', 'random', 'guided']:
+            # for expert in [50, 85]:
+            #     aug = f'{aug}_{expert}'
+                aug = f'{aug}'
+                for gap in [5]:
+                        for nl in [1,2]:
+                            for hd in [256]:
+                                if aug == 'no_aug':
+                                    dataset_name = None
+                                else:
+                                    dataset_name = f'/staging/ncorrado/datasets/{env_id}/{aug}/m_1.hdf5'
 
-    for m in [1]:
-        for env_id in ['maze2d-umaze-v1', 'maze2d-medium-v1', 'maze2d-large-v1',]:# 'antmaze-umaze-diverse-v1', 'antmaze-medium-diverse-v1', 'antmaze-large-diverse-v1']:
-        # for env_id in ['antmaze-umaze-diverse-v1', 'antmaze-medium-diverse-v1', 'antmaze-large-diverse-v1']:
-            for aug in ['no_aug', 'random', 'guided', 'mixed']:
-                for policy_lr in [3e-4, 3e-5]:
-                    for qf_lr in [3e-4]:
-                        for gap in [5]:
-                            # for n_layers in [2]:
-                            #     for hidden_dims in [256]:
-                            m = 1
+                                save_dir = f'results/{env_id}/{aug}/cql/nl_{nl}/gap_{gap}'
+                                hidden_dims = 256
 
-                            if aug == 'no_aug':
-                                save_dir = f'results/{aug}/{env_id}/cql/lr_{policy_lr}/lr_{qf_lr}/g_{gap}'
-                                dataset_name = None
-                                batch_size = 256
-                            else:
-                                save_dir = f'results/{aug}/{env_id}/cql/lr_{policy_lr}/lr_{qf_lr}/g_{gap}'
-                                dataset_name = f'/staging/ncorrado/datasets/{env_id}/{aug}/m_{m}.hdf5'
-                                batch_size = 256
+                                max_timesteps = int(1e6) if nl == 2 else int(1e6)
+                                eval_freq = int(20e3)
 
-                            max_timesteps = int(1e6)
-                            eval_freq = int(20e3)
-                            command = f'python -u algorithms/cql.py --max_timesteps {max_timesteps} --eval_freq {eval_freq}' \
-                                      f' --save_dir {save_dir} ' \
-                                      f' --env {env_id}' \
-                                      f' --qf_lr {qf_lr}' \
-                                      f' --policy_lr {policy_lr}' \
-                                      f' --cql_target_action_gap {gap}' \
-                                      f' --batch_size {batch_size}' \
-                                      # f' --cql_min_q_weight 5'
+                                command = f'python -u algorithms/cql.py --max_timesteps {max_timesteps} --eval_freq {eval_freq}' \
+                                          f' --save_dir {save_dir} ' \
+                                          f' --env {env_id}' \
+                                          f' --cql_target_action_gap {gap}' \
+                                          f' --cql_min_q_weight 5' \
+                                          f' --n_layers {nl} --hidden_dims {hidden_dims}'
+                                          # f' --qf_lr {qf_lr}' \
+                                          # f' --policy_lr {policy_lr}' \
 
-                            if dataset_name:
-                                command += f' --dataset_name {dataset_name}'
+                                          # f' --batch_size {batch_size}' \
 
-                            # command = '4,9,' + command + f' --device cuda'
-                            # command = command.replace(' ', '*')
+                                if dataset_name:
+                                    command += f' --dataset_name {dataset_name}'
 
-                            mem, disk = MEMDISK[m][env_id]
-                            command = f'{mem},{disk},' + command.replace(' ', '*')
-                            # print(command)
-                            print(command)
-                            i+=1
-                            all_commands += command + '\n'
+                                mem, disk = MEMDISK[1][env_id]
+                                memdisk = f'{mem},{disk},'
+                                command = memdisk + command.replace(' ', '*')
+                                print(command)
+                                i+=1
+                                all_commands += command + '\n'
--- src/viz/maze.py
+++ src/viz/maze.py
@@ -9,7 +9,9 @@ from matplotlib import pyplot as plt
 from algorithms.utils import load_dataset
 
 
-for env_id in ['maze2d-umaze-v1', 'maze2d-medium-v1', 'maze2d-large-v1']:
+# for env_id in ['maze2d-umaze-v1', 'maze2d-medium-v1', 'maze2d-large-v1']:
+for env_id in ['maze2d-umaze-v1']:
+
     for aug in ['random', 'guided']:
         plt.figure(figsize=(12, 12))
 
@@ -21,23 +23,23 @@ for env_id in ['maze2d-umaze-v1', 'maze2d-medium-v1', 'maze2d-large-v1']:
         for key in data_hdf5.keys():
             dataset[key] = np.array(data_hdf5[key])
 
-        # plot no_aug
-        start = int(0e6)
-        end = start + int(5e3)
-
-        observations = dataset['observations'][start:end]
-        next_observations = dataset['next_observations'][start:end]
-        rewards = dataset['rewards'][start:end]
-        at_goal = rewards > 0
-
-        x = observations[:, 0]
-        y = observations[:, 1]
-        plt.scatter(x, y, alpha=0.5)
-        plt.scatter(x[at_goal], y[at_goal], alpha=0.5, color='g')
+        # # plot no_aug
+        # start = int(0e6)
+        # end = start + int(5e3)
+        #
+        # observations = dataset['observations'][start:end]
+        # next_observations = dataset['next_observations'][start:end]
+        # rewards = dataset['rewards'][start:end]
+        # at_goal = rewards > 0
+        #
+        # x = observations[:, 0]
+        # y = observations[:, 1]
+        # plt.scatter(x, y, alpha=0.5)
+        # plt.scatter(x[at_goal], y[at_goal], alpha=0.5, color='g')
 
         # plot aug
-        start = len(dataset['observations']) - int(3e3)
-        end = start + int(3e3)
+        start = len(dataset['observations']) - int(1e3)
+        end = start + int(1e3)
         observations = dataset['observations'][start:end]
         next_observations = dataset['next_observations'][start:end]
         rewards = dataset['rewards'][start:end]
@@ -50,19 +52,20 @@ for env_id in ['maze2d-umaze-v1', 'maze2d-medium-v1', 'maze2d-large-v1']:
 
         u = next_x - x
         v = next_y - y
-        norm = np.sqrt(u**2 + v**2)
-        u /= norm
-        v /= norm
+        # u *= 2
+        # v *= 2
+        # norm = np.sqrt(u**2 + v**2)
+        # u /= norm
+        # v /= norm
 
-        plt.quiver(x, y, u, v)
-        plt.scatter(next_x[at_goal],next_y[at_goal], alpha=0.5, color='g')
-
-        plt.title(f'{env_id}: {aug}', fontsize=24)
-        plt.xlabel('x position', fontsize=24)
-        plt.ylabel('y position', fontsize=24)
-        plt.xticks(fontsize=20)
-        plt.yticks(fontsize=20)
+        plt.quiver(x, y, u, v, scale=3, width=0.005)
+        # plt.scatter(next_x[at_goal],next_y[at_goal], alpha=0.5, color='g')
+        plt.title(f'{env_id}\n{aug.capitalize()} Augmentation', fontsize=56)
+        plt.xlabel('x position', fontsize=56)
+        plt.ylabel('y position', fontsize=56)
+        plt.xticks(fontsize=48)
+        plt.yticks(fontsize=48)
         plt.tight_layout()
+        os.makedirs(f'figures/{env_id}', exist_ok=True)
+        plt.savefig(f'figures/{env_id}/{aug}.png')
         plt.show()
-        # os.makedirs(f'figures/{env_id}', exist_ok=True)
-        # plt.savefig(f'figures/{env_id}/{aug}.png')
