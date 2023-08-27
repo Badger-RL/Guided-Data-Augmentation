7980c01127c13f307f543f232ad854b8aafbf97e
CohneQu <yuxiao1155@gmail.com>
2023-08-27 13:56:51 -0400

    update discrete_BCQ to solve CartPole-v1

--- src/algorithms/discrete_BCQ/utils.py
+++ src/algorithms/discrete_BCQ/utils.py
@@ -1,5 +1,4 @@
 import gym
-import d4rl
 import numpy as np
 import torch
 
@@ -56,33 +55,23 @@ class StandardBuffer(object):
 
 
 	def load(self, save_folder, size=-1):
-		# reward_buffer = np.load(f"{save_folder}_reward.npy")
-		env = gym.make("maze2d-umaze-v1")
-		dataset = d4rl.qlearning_dataset(env)
-		reward_buffer = dataset['rewards']
-		self.state = dataset['observations']
-		self.action = dataset['actions']
-		self.next_state = dataset['next_observations']
-		self.reward = dataset['rewards']
-		self.not_done = dataset['terminals']
-
+		reward_buffer = np.load(f"{save_folder}_reward.npy")
 		
-		# # Adjust crt_size if we're using a custom size
+		# Adjust crt_size if we're using a custom size
 		size = min(int(size), self.max_size) if size > 0 else self.max_size
 		self.crt_size = min(reward_buffer.shape[0], size)
 
-		# self.state[:self.crt_size] = np.load(f"{save_folder}_state.npy")[:self.crt_size]
-		# self.action[:self.crt_size] = np.load(f"{save_folder}_action.npy")[:self.crt_size]
-		# self.next_state[:self.crt_size] = np.load(f"{save_folder}_next_state.npy")[:self.crt_size]
-		# self.reward[:self.crt_size] = reward_buffer[:self.crt_size]
-		# self.not_done[:self.crt_size] = np.load(f"{save_folder}_not_done.npy")[:self.crt_size]
+		self.state[:self.crt_size] = np.load(f"{save_folder}_state.npy")[:self.crt_size]
+		self.action[:self.crt_size] = np.load(f"{save_folder}_action.npy")[:self.crt_size]
+		self.next_state[:self.crt_size] = np.load(f"{save_folder}_next_state.npy")[:self.crt_size]
+		self.reward[:self.crt_size] = reward_buffer[:self.crt_size]
+		self.not_done[:self.crt_size] = np.load(f"{save_folder}_not_done.npy")[:self.crt_size]
 
 		print(f"Replay Buffer loaded with {self.crt_size} elements.")
 
+
 # Create environment, add wrapper if necessary and create env_properties
 def make_env(env_name, atari_preprocessing):
-	print(env_name)
-	
 	env = gym.make(env_name)
 	
 	is_atari = gym.envs.registry.spec(env_name).entry_point == 'gym.envs.atari:AtariEnv'
@@ -97,5 +86,5 @@ def make_env(env_name, atari_preprocessing):
 		env,
 		is_atari,
 		state_dim,
-		env.action_space.shape[0]
+		env.action_space.n
 	)
