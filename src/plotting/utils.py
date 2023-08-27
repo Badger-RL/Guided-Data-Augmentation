5cca789b2c79dd41a4d5392357fd86143291e6be
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-08-27 12:36:49 -0500

    print return and ci; use dot-dash for guided_Neg

--- src/plotting/utils.py
+++ src/plotting/utils.py
@@ -50,6 +50,7 @@ def plot(path_dict, field_name='return'):
         ci = 1.96 * std / np.sqrt(N)
         q05 = avg_of_avgs + ci
         q95 = avg_of_avgs - ci
+        print(agent, avg_of_avgs[-1], ci[-1])
 
         style_kwargs = {}
         style_kwargs['linewidth'] = 3
@@ -58,7 +59,8 @@ def plot(path_dict, field_name='return'):
             style_kwargs['linestyle'] = ':'
         elif 'random' in agent.lower():
             style_kwargs['linestyle'] = '--'
-
+        elif 'guided_neg' in agent.lower():
+            style_kwargs['linestyle'] = '-.'
         # t = np.arange(len(avg_of_avgs)) * 5000
         plt.plot(t, avg_of_avgs, label=agent, **style_kwargs)
         plt.fill_between(t, q05, q95, alpha=0.2)
