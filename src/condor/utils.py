019d1a1e4cb7b78b298f923fc96c206b86acff55
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-09-02 01:35:53 -0500

    final bc

--- src/condor/utils.py
+++ src/condor/utils.py
@@ -1,18 +1,19 @@
 # Memory and disk requirements for each task. The key corresponds to the augmentation ratio, m.
 MEMDISK = {
     1: {
-        'maze2d-umaze-v1': (1.6, 9),
-        'maze2d-medium-v1': (1.6, 9),
-        'maze2d-large-v1': (1.6, 9),
+        'maze2d-umaze-v1': (1.5, 9),
+        'maze2d-medium-v1': (1.5, 9),
+        'maze2d-large-v1': (1.5, 9),
 
-        'antmaze-umaze-diverse-v1': (2.2, 9),
-        'antmaze-medium-diverse-v1': (2.2, 9),
-        'antmaze-large-diverse-v1': (3.3, 9),
+        'antmaze-umaze-diverse-v1': (1.5, 9),
+        'antmaze-medium-diverse-v1': (1.5, 9),
+        'antmaze-large-diverse-v1': (1.5, 9),
 
         'PushBallToGoal-v0': (1.4, 9),
         'PushBallToGoal-v1': (1.4, 9),
         'PushBallToGoal-v2': (1.4, 9),
         'PushBallToGoalEasy-v0': (1.4, 9),
+        'PushBallToGoalHard-v0': (1.4, 9),
 
     },
     2: {
