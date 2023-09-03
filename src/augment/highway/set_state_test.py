3c0059b648541879a667402dad01744356079b1c
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-09-03 15:00:04 -0500

    bug fix; wrong angle dim

--- src/augment/highway/set_state_test.py
+++ src/augment/highway/set_state_test.py
@@ -13,8 +13,8 @@ plt.show()
 theta = np.pi/4
 obs["observation"][0] = 26
 obs["observation"][1] = 15
-obs["observation"][3] = np.cos(theta)
-obs["observation"][4] = np.sin(theta)
+obs["observation"][4] = np.cos(theta)
+obs["observation"][5] = np.sin(theta)
 env.set_state(obs)
 
 plt.imshow(env.render())
@@ -25,8 +25,8 @@ theta = -np.pi/4
 obs = obs["observation"]
 obs[0] = -26
 obs[1] = -15
-obs[3] = np.cos(theta)
-obs[4] = np.sin(theta)
+obs[4] = np.cos(theta)
+obs[5] = np.sin(theta)
 env.set_state(obs)
 
 plt.imshow(env.render())
