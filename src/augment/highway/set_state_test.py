cd8f00e2d3a6994c597293470067c21e466b3e9b
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-09-03 14:44:37 -0500

    set_state now updates heading angle

--- src/augment/highway/set_state_test.py
+++ src/augment/highway/set_state_test.py
@@ -1,4 +1,5 @@
 import gymnasium as gym
+import numpy as np
 from matplotlib import pyplot as plt
 
 env = gym.make('parking-v0', render_mode='rgb_array')
@@ -9,17 +10,23 @@ plt.imshow(env.render())
 plt.show()
 
 # set_state() should work with dict observations
+theta = np.pi/4
 obs["observation"][0] = 26
 obs["observation"][1] = 15
+obs["observation"][3] = np.cos(theta)
+obs["observation"][4] = np.sin(theta)
 env.set_state(obs)
 
 plt.imshow(env.render())
 plt.show()
 
 # set_state() should also work with np.array observations
+theta = -np.pi/4
 obs = obs["observation"]
 obs[0] = -26
 obs[1] = -15
+obs[3] = np.cos(theta)
+obs[4] = np.sin(theta)
 env.set_state(obs)
 
 plt.imshow(env.render())
