a7a90a7ac312ccc87d158a0e08524a3d0373b7ea
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-09-06 11:48:40 -0500

    remove obs scaling

--- src/augment/highway/parking_augment_visulization.py
+++ src/augment/highway/parking_augment_visulization.py
@@ -7,6 +7,8 @@ import h5py
 
 
 dataset_path = f"/Users/yxqu/Desktop/Research/GuDA/GuidedDataAugmentationForRobotics/src/datasets/parking-v0/no_aug.hdf5"
+dataset_path = f"../../datasets/parking-v0/no_aug.hdf5"
+dataset_path = f"tmp.hdf5"
 observed_dataset = load_dataset(dataset_path)
 
 n = len(observed_dataset['observations'])
@@ -14,10 +16,6 @@ env = gym.make('parking-v0', render_mode='rgb_array')
 env.reset()
 for i in range(n):
     obs = observed_dataset['observations'][i]
-    obs[0] *= 100
-    obs[1] *= 100
-    obs[2] *= 5
-    obs[3] *= 5
     env.set_state(observed_dataset['observations'][i])
     env.render()
     time.sleep(0.1)
