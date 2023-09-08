bug fix, rotation stilll not working

src/augment/antmaze/antmaze_aug_function.py
@@ -491,7 +491,7 @@ class AntMazeTrajectoryGuidedAugmentationFunction(AntMazeAugmentationFunction):
             return 0
         elif location in [(3, 1), (3, 2)]: # up
             return np.pi/2
-        elif location in  [(2, 3), (3, 3)]: # left
+        elif location in [(2, 3), (3, 3)]: # left
             return np.pi
 
 
@@ -660,9 +660,18 @@ class AntMazeTrajectoryGuidedAugmentationFunction(AntMazeAugmentationFunction):
             if is_new_trajectory:
                 init_obs = trajectory['observations'][i]
                 final_obs = trajectory['next_observations'][-1]
+                delta = final_obs[:2] - init_obs[:2]
+                if np.linalg.norm(delta) < 2:
+                    valid_locations = self.get_valid_locations(0)
+                    idx = np.random.choice(len(valid_locations))
+                    aug_location = np.array(valid_locations[idx]).astype(self.env.observation_space.dtype)
+                    boundary = self._get_valid_boundaries(*aug_location)
+                    new_pos = self._sample_from_box(*boundary)
+                else:
+                    new_pos, aug_location = self._sample_pos()
+
                 # direction = self.get_direction(init_obs, final_obs)
                 # print(direction)
-                new_pos, aug_location = self._sample_pos()
                 delta_pos = new_pos - init_obs[:2]
 
             augmented_obs = observation.copy()

src/generate/generate_antmaze_augment_dataset_large.py
@@ -28,8 +28,8 @@ timestamps = {
 
 for maze in ['umaze',]:
     for aug in ['guided']:
-        for demo_size in [50e3]:
-            aug_size = int(1e6)
+        for demo_size in [10e3]:
+            aug_size = int(100e3)
             demo_size = int(demo_size)
 
             env_id = f'antmaze-{maze}-diverse-v1'
@@ -48,8 +48,9 @@ for maze in ['umaze',]:
 
             n = len(to_aug_dataset['rewards'])
 
-            start_timestamps = np.arange(0, n - 30, 30)
-            end_timestamps = np.arange(30, n, 30)
+            subtraj_len = 30
+            start_timestamps = np.arange(0, n - subtraj_len, subtraj_len)
+            end_timestamps = np.arange(subtraj_len, n, subtraj_len)
             directions = np.zeros_like(start_timestamps)
 
             num_of_trajectories = len(start_timestamps)